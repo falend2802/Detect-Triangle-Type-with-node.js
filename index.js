a5a773f14eef7d29dc26d6acafcd40aa4b7e2be4
Moh Falend Wandamen <62651106+falend2802@users.noreply.github.com>
2022-06-16 19:25:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const detectTriangle = require("./detectTriangle.js");
+
+
+console.log(detectTriangle(1, 1, 1));
+console.log(detectTriangle(1, 2, 1));
+console.log(detectTriangle(1, 2, 3));
+console.log(detectTriangle(false, 1, 1));
+console.log(detectTriangle(1, false, 1));
+console.log(detectTriangle(1, 1, false));
